446d29618b095777bc9ff292f86de3b9e3673e57
Ivan Z <corvuss90@gmail.com>
2020-11-10 23:40:22 +0300

    Delete unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,10 @@
-import Link from 'next/link'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { exampleOperations } from '../redux/ducks/example'
-import { GetServerSideProps } from 'next'
-
-const Index = ({ users }) => {
-
-  const dispatch = useDispatch()
-  const setUsers = (data) => dispatch(exampleOperations.setUsers(data))
-  
-  useEffect(() => {
-    setUsers(users)
-  }, [dispatch, users])
+const Index = () => {
 
   return (
     <>
       <h1>Index</h1>
-      <Link href='/about'>About</Link>
     </>
   )
-}
-
-export async function getServerSideProps() {
-  // Fetch data from external API
-  let data = []
-
-  try {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/users`)
-    data = await res.json()
-  } catch (err) {
-    console.error(err)
-  }
-
-  // Pass data to the page via props
-  return { props: { users: data }}
 }
 
 export default Index
