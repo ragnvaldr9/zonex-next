fe6a43f3e4315a3891473a04874e34b2adb2c0af
Ivan Z <corvuss90@gmail.com>
2020-11-26 23:05:54 +0300

    [header]: desktop markup

--- components/shared/Header/HeaderTools.tsx
+++ components/shared/Header/HeaderTools.tsx
@@ -1,8 +1,33 @@
+import Link from 'next/link'
+import { memo } from 'react'
 import css from './header.module.scss'
+
+const CartLink = memo(() => {
+  return (
+    <Link href={'/'} passHref>
+      <a className={`${css.tool} ${css.cart}`}>
+        <span className={css.counter}>0</span>
+      </a>
+    </Link>
+  )
+})
 
 const HeaderTools = () => {
   return (
-    <ul></ul>
+    <ul className={css.tools}>
+      <li className={css.toolsItem}>
+        {/* Search will be added later */}
+        <span className={`${css.tool} ${css.search}`}></span>
+      </li>
+      <li className={css.toolsItem}>
+        <Link href={'/'} passHref>
+          <a className={`${css.tool} ${css.profile}`}></a>
+        </Link>
+      </li>
+      <li className={css.toolsItem}>
+        <CartLink />
+      </li>
+    </ul>
   )
 }
 
