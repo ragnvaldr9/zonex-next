5a885b5aa56903ac5d75eb255952e9091fd40954
Ivan Z <corvuss90@gmail.com>
2020-11-26 21:46:01 +0300

    [dev]: remove unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,7 @@
 import { GetServerSideProps } from 'next'
 import Header from '../components/shared/Header/Header'
-import Product from '../components/shared/Product/Product'
-import Button from '../components/_ui-elements/Button/Button'
-import LoadMore from '../components/_ui-elements/LoadMore/LoadMore'
-import MainLink from '../components/_ui-elements/MainLink/MainLik'
-import SVGSprite from '../components/_ui-elements/SVGSprite/SVGSprite'
 import { detectInitialDeviceType } from '../utils/detectInitialDeviceType'
-import { mainNav, testProduct } from '../utils/testData'
+import { mainNav } from '../utils/testData'
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   
@@ -28,9 +23,6 @@ const Index = () => {
   return (
     <>
       <Header navData={mainNav} />
-      <div style={{ width: '260px' }}>
-        <LoadMore />
-      </div>
     </>
   )
 }
--- redux/ducks/index.js
+++ redux/ducks/index.js
@@ -1,2 +1,1 @@
-export { default as example } from './example'
 export { default as device } from './device'
