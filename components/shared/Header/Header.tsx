f2bc75382c2933f814625ca3961ee1be86d4a66f
Ivan Z <corvuss90@gmail.com>
2020-11-26 21:46:01 +0300

    [dev]: remove unused code

--- components/shared/Header/Header.tsx
+++ components/shared/Header/Header.tsx
@@ -1,23 +1,17 @@
 import { TNavItem } from './headerTypes'
-
 import css from './header.module.scss'
 import HeaderNav from './HeaderNav'
 import clsx from 'clsx'
-import useResponsive from '../../_utils/Responsive/useResponsive'
 import Responsive from '../../_utils/Responsive/Responsive'
 
 
 const Header: React.FC<{navData: Array<TNavItem>}> = ({ navData }) => {
-
-  // const { isMobile } = useResponsive()
-
   return (
     <header className={css.header}>
       <div className={clsx(css.headerInner, 'container', 'container-narrow ')}>
-        {/* {!isMobile && <HeaderNav navData={navData} />} */}
-        <Responsive renderOn={['isMobile']}>
-          <div style={{width: '200px', height: '200px', background: '#000'}}></div>
-        </Responsive>
+        {/* <Responsive renderOn={['isMobile']}>
+            <HeaderNav navData={navData} />
+        </Responsive> */}
       </div>
     </header>
   )
