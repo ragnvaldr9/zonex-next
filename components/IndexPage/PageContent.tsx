4feb375b300a156950b668bdbc050da796c7a232
Ivan Zotov <corvuss90@gmail.com>
2020-12-23 22:52:42 +0300

    [index-page]: banners section

--- components/IndexPage/PageContent.tsx
+++ components/IndexPage/PageContent.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react"
+import Banners from "./Banners/Banners"
 import IndexHero from "./IndexHero/IndexHero"
 
 const IndexPageContent: FC = () => {
   return ( 
     <main>
       <IndexHero />
+      <Banners />
     </main> 
   )
 }
