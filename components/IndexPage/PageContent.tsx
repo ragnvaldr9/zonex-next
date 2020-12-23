524d1052f7e89dd624e38a391cc481bd512d4e76
Ivan Zotov <corvuss90@gmail.com>
2020-12-23 23:48:11 +0300

    [index-page]: main page products section

--- components/IndexPage/PageContent.tsx
+++ components/IndexPage/PageContent.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react"
 import Banners from "./Banners/Banners"
 import IndexHero from "./IndexHero/IndexHero"
+import MainProducts from "./MainProducts/MainProducts"
 
 const IndexPageContent: FC = () => {
   return ( 
     <main>
       <IndexHero />
       <Banners />
+      <MainProducts />
     </main> 
   )
 }
--- components/_ui-elements/MainLink/MainLik.tsx
+++ components/_ui-elements/MainLink/MainLik.tsx
@@ -1,18 +1,21 @@
+import clsx from 'clsx'
 import Link from 'next/link'
+import { memo } from 'react'
 import css from './mainLink.module.scss'
 
 type TMainLink = {
   children: string
   to: string
   active?: boolean
+  classes?: string
 }
 
-const MainLink: React.FC<TMainLink> = ({ children, to = '#', active = false }) => {
+const MainLink: React.FC<TMainLink> = memo(({ children, to = '#', active = false, classes }) => {
   return (
     <Link href={to} passHref>
-      <a className={css.mainLink}>{children}</a>
+      <a className={clsx(css.mainLink, classes && classes)}>{children}</a>
     </Link>
   )
-}
+})
 
 export default MainLink
